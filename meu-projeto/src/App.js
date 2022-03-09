1e5dcfd57897e2ab5ca5fc4e42d7b87f1c6a72a6
Rafael Pereira <94644258+rafaelpssilva@users.noreply.github.com>
2022-03-09 04:04:06 -0300

    Aula 03 - Entendendo o JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const name = 'Rafael'
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(9, 10)}</p>
+      <img src={url} alt="Minha Imagem" />
     </div>
   );
 }
